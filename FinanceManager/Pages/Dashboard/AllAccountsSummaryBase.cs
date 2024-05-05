c1644e8714a7c49ced44bfb4a8d6bcb1d93dce6a
MikiKarkowski <mikikarkowski@gmail.com>
2024-05-05 21:55:31 +0200

    #6 Added AllAccountsSummaryBase

---
+++ FinanceManager/Pages/Dashboard/AllAccountsSummaryBase.cs
@@ -0,0 +1,33 @@
+﻿using FinanceManager.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FinanceManager.Pages.Dashboard
+{
+    public class AllAccountsSummaryBase : ComponentBase
+    {
+        private Random random = new Random();
+        [Parameter]
+        public Dictionary<string, List<AccountEntry>> Accounts { get; set; }
+
+        public double Spent { get; set; }
+        public double Invested { get; set; }
+        public double FundsLeft { get; set; }
+        public double AllFunds { get; set; }
+        public double SpentOnCar { get; set; }
+        public double DayToDay { get; set; }
+        public double Investments { get; set; }
+        public double Rest { get; set; }
+        
+        protected override async Task OnParametersSetAsync()
+        {
+            Spent = Math.Round(random.NextDouble(), 2);
+            Invested = Math.Round(random.NextDouble(), 2);
+            FundsLeft = Math.Round(random.NextDouble(), 2);
+            AllFunds = Math.Round(random.NextDouble(), 2);
+            SpentOnCar = Math.Round(random.NextDouble(), 2);
+            DayToDay = Math.Round(random.NextDouble(), 2);
+            Investments = Math.Round(random.NextDouble(), 2);
+            Rest = Math.Round(random.NextDouble(), 2);
+        }
+    }
+}
