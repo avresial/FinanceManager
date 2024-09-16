d4bb539b0127ef7451bc32d701f594a7a1924362
Mikołaj Karkowski <mikikarkowski@gmail.com>
2024-09-16 22:17:29 +0200

    Dashboard update

--- FinanceManager/Program.cs
+++ FinanceManager/Program.cs
@@ -8,14 +8,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddApplication().AddInfrastructure();
 
-builder.Services.AddOidcAuthentication(options =>
-{
-	// Configure your authentication provider options here.
-	// For more information, see https://aka.ms/blazor-standalone-auth
-	builder.Configuration.Bind("Local", options.ProviderOptions);
-});
+//builder.Services.AddOidcAuthentication(options =>
+//{
+//	// Configure your authentication provider options here.
+//	// For more information, see https://aka.ms/blazor-standalone-auth
+//	builder.Configuration.Bind("Local", options.ProviderOptions);
+//});
 
 await builder.Build().RunAsync();
