02e4c9513e43b183243bd66e20a8309a5351d06a
Mikołaj Karkowski <mikikarkowski@gmail.com>
2024-09-17 23:57:46 +0200

    Added time span buttons to dashboard

---
+++ FinanceManager.Presentation/Components/Dashboard/Dashboard.razor.cs
@@ -0,0 +1,43 @@
+using FinanceManager.Core.Entities;
+using FinanceManager.Core.Repositories;
+using Microsoft.AspNetCore.Components;
+using System.Collections.ObjectModel;
+
+namespace FinanceManager.Presentation.Components.Dashboard
+{
+	public partial class Dashboard : ComponentBase
+	{
+		const int UnitHeight = 125;
+		[Inject]
+		public IBankAccountRepository AccountsService { get; set; }
+
+
+		public List<BankAccount> Accounts;
+		public ObservableCollection<BankAccountEntry> AccountEntries { get; set; }
+
+		public DateTime StartDateTime { get; set; }
+		public string AccountName { get; set; }
+		public bool isLoading { get; set; }
+		public string ErrorMessage { get; set; } = string.Empty;
+
+		protected override async Task OnInitializedAsync()
+		{
+			await GetThisMonth();
+		}
+
+		public async Task GetAllTime()
+		{
+			StartDateTime = new DateTime();
+		}
+		public async Task GetThisMonth()
+		{
+			StartDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+		}
+
+		public async Task GetThisYear()
+		{
+			StartDateTime = new DateTime(DateTime.Now.Year, 1, 1);
+		}
+
+	}
+}
