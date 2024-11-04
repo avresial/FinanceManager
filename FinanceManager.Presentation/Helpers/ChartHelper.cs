4c7af901df03877e4dbd294e19976117bc98b1cf
MikiKarkowski <mikikarkowski@gmail.com>
2024-11-04 23:40:20 +0100

    #23 Added ChartHelper, implemented formatter in 5 cards

---
+++ FinanceManager.Presentation/Helpers/ChartHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceManager.Presentation.Helpers
+{
+    internal static class ChartHelper
+    {
+        public static string GetCurrencyFormatter(string currency)
+        {
+            return @"function(value, opts) {
+                    if (value === undefined) {return '';}
+                    return Number(value).toLocaleString() + " + $" ' {currency}' " + ";}";
+        }
+    }
+}
