a22a75e67328b934b636f0ad778d76a3fb3279a0
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-05-05 20:43:43 +0200

    #76 Update for admin dashboard

--- code/FinanceManager/Pages/Admin/AdminDashboard.razor.cs
+++ code/FinanceManager/Pages/Admin/AdminDashboard.razor.cs
@@ -1,5 +1,29 @@
-namespace FinanceManager.WebUi.Pages.Admin;
-public partial class AdminPage
-{
+using FinanceManager.Components.Services;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
+namespace FinanceManager.WebUi.Pages.Admin;
+public partial class AdminDashboard
+{
+    private int _userCount = 0;
+    private ChartOptions _axisChartOptions = new ChartOptions()
+    {
+        ChartPalette = ["#ffab00", "#34C759", "#007AFF"],
+        ShowLabels = false,
+    };
+
+    private List<ChartSeries> _series = new()
+    {
+        new ChartSeries() { Name = "Users count", Data = new double[] {1,2,3,4,5,10,20,30 } },
+    };
+
+    [Inject] required public AdministrationUsersService AdministrationUsersService { get; set; }
+
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        var userCount = await AdministrationUsersService.GetUsersCount();
+        _userCount = userCount is null ? 0 : userCount.Value;
+    }
 }
