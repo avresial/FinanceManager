b0ccb197337378508c96e6e813260b7f2e52446e
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-06-08 17:47:01 +0200

    Removed ApexChart from IncomeVsSpendingOverviewCard

--- code/FinanceManager.Components/Components/Dashboard/Cards/IncomeVsSpendingOverviewCard.razor.cs
+++ code/FinanceManager.Components/Components/Dashboard/Cards/IncomeVsSpendingOverviewCard.razor.cs
@@ -1,10 +1,10 @@
-using ApexCharts;
-using FinanceManager.Components.Helpers;
+using FinanceManager.Components.Services;
 using FinanceManager.Domain.Entities.Login;
 using FinanceManager.Domain.Entities.MoneyFlowModels;
 using FinanceManager.Domain.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
+using MudBlazor;
 
 namespace FinanceManager.Components.Components.Dashboard.Cards;
 
@@ -12,105 +12,81 @@ public partial class IncomeVsSpendingOverviewCard
 {
     private UserSession? _user;
 
-    private ApexChart<IncomeVsSpendingEntry>? _chart;
-
-    private ApexChartOptions<IncomeVsSpendingEntry> _options = new()
-    {
-        Colors = new List<string> { "#00FF00", "#FF0000" },
-        Fill = new Fill
-        {
-            Type = new List<FillType> { FillType.Gradient, FillType.Gradient },
-            Gradient = new FillGradient
-            {
-                ShadeIntensity = 1,
-                OpacityFrom = 0.2,
-                OpacityTo = 0.9,
-
-            },
-        },
-    };
-
     [Parameter] public string Height { get; set; } = "300px";
     [Parameter] public DateTime StartDateTime { get; set; }
 
     [Inject] public required IMoneyFlowService MoneyFlowService { get; set; }
     [Inject] public required ILogger<IncomeVsSpendingOverviewCard> Logger { get; set; }
+    [Inject] public required IFinancialAccountService FinancalAccountService { get; set; }
+    [Inject] public required ISettingsService settingsService { get; set; }
+    [Inject] public required ILoginService loginService { get; set; }
 
     public List<IncomeVsSpendingEntry> ChartData { get; set; } = [];
 
     protected override void OnInitialized()
     {
-        _options.Tooltip = new ApexCharts.Tooltip
-        {
-            Y = new TooltipY
-            {
-                Formatter = ChartHelper.GetCurrencyFormatter(settingsService.GetCurrency())
-            }
-        };
-        _options.Chart = new Chart
-        {
-            Toolbar = new ApexCharts.Toolbar
-            {
-                Show = false
-            },
-            Zoom = new Zoom()
-            {
-                Enabled = false
-            },
-            Sparkline = new ChartSparkline()
-            {
-                Enabled = true,
-            },
-        };
+        //ChartHelper.GetCurrencyFormatter(settingsService.GetCurrency())
 
-
-        _options.Xaxis = new XAxis()
-        {
-            AxisTicks = new AxisTicks()
-            {
-                Show = false,
-            },
-            AxisBorder = new AxisBorder()
-            {
-                Show = false
-            },
-            Position = XAxisPosition.Bottom,
-            Type = XAxisType.Datetime
-
-        };
-
-        _options.Yaxis = new List<YAxis>();
-
-        _options.Yaxis.Add(new YAxis
-        {
-            AxisTicks = new AxisTicks()
-            {
-                Show = false
-            },
-            Show = false,
-            SeriesName = "NetValue",
-            DecimalsInFloat = 0,
-
-        });
-        _options.Colors = new List<string>
-        {
-            "#B2BF84",
-            "#D93D3D",
-        };
+        //_options.Colors = new List<string>
+        //{
+        //    "#B2BF84",
+        //    "#D93D3D",
+        //};
     }
 
+    private ChartOptions _options = new ChartOptions
+    {
+        LineStrokeWidth = 3,
+
+        ChartPalette = ["#B2BF84", "#D93D3D", "#ffab00"],
+        ShowLegend = false,
+    };
+
+
+
+    private AxisChartOptions _axisChartOptions = new AxisChartOptions()
+    {
+        MatchBoundsToSize = true,
+    };
+
+    private List<ChartSeries> series = [];
+    private ChartSeries? incomeSerie = null;
 
     protected override async Task OnParametersSetAsync()
     {
         _user = await loginService.GetLoggedUser();
         if (_user is null) return;
+        series.Clear();
 
-        ChartData.Clear();
-        ChartData.AddRange(await GetData());
-
-        if (_chart is not null) await _chart.UpdateSeriesAsync(true);
+        series.Add(await GetIncomeSeries());
+        series.Add(await GetSpendingSeries());
     }
 
+    private async Task<ChartSeries> GetIncomeSeries()
+    {
+        TimeSpan timeSeriesStep = new TimeSpan(1, 0, 0, 0);
+        DateTime end = DateTime.UtcNow;
+
+        return new()
+        {
+            Name = "Income",
+
+            Data = (await MoneyFlowService.GetIncome(_user!.UserId, StartDateTime.Date, end, timeSeriesStep)).Select(x => (double)x.Value).ToArray()
+        };
+
+    }
+
+    private async Task<ChartSeries> GetSpendingSeries()
+    {
+        TimeSpan timeSeriesStep = new TimeSpan(1, 0, 0, 0);
+        DateTime end = DateTime.UtcNow;
+
+        return new()
+        {
+            Name = "Spending",
+            Data = (await MoneyFlowService.GetSpending(_user!.UserId, StartDateTime.Date, end, timeSeriesStep)).Select(x => (double)x.Value).ToArray()
+        };
+    }
     private async Task<List<IncomeVsSpendingEntry>> GetData()
     {
         List<IncomeVsSpendingEntry> result = [];
@@ -139,22 +115,29 @@ public partial class IncomeVsSpendingOverviewCard
 
         if (income.Count == 0 && spending.Count == 0) return [];
 
+
+
+
         for (var date = end; date >= StartDateTime; date = date.Add(-timeSeriesStep))
         {
-            result.Add(new IncomeVsSpendingEntry
-            {
-                Date = date,
-                Income = income.FirstOrDefault(x => x.DateTime == date)?.Value ?? 0,
-                Spending = spending.FirstOrDefault(x => x.DateTime == date)?.Value ?? 0
-            });
+            //result.Add(new IncomeVsSpendingEntry
+            //{
+            //    Date = date,
+            //    Income = income.FirstOrDefault(x => x.DateTime == date)?.Value ?? 0,
+            //    Spending = spending.FirstOrDefault(x => x.DateTime == date)?.Value ?? 0
+            //});
         }
+
 
         return result;
     }
 }
+
+
 public class IncomeVsSpendingEntry
 {
     public DateTime Date;
     public decimal Income;
     public decimal Spending;
+    public decimal Balance;
 }
