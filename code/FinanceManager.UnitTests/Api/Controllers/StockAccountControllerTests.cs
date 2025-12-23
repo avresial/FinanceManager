e90cf4abbb0b81162611070a2ec0242fbcfbaf4e
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-12-23 22:37:41 +0100

    Unit test fix

--- code/FinanceManager.UnitTests/Api/Controllers/StockAccountControllerTests.cs
+++ code/FinanceManager.UnitTests/Api/Controllers/StockAccountControllerTests.cs
@@ -15,6 +15,7 @@ public class StockAccountControllerTests
     private readonly Mock<IAccountRepository<StockAccount>> _mockStockAccountRepository;
     private readonly Mock<IStockAccountEntryRepository<StockAccountEntry>> _mockStockAccountEntryRepository;
     private readonly StockAccountController _controller;
+    private const int TestUserId = 1;
 
     public StockAccountControllerTests()
     {
@@ -25,7 +26,7 @@ public class StockAccountControllerTests
         // Mock user identity
         var user = new ClaimsPrincipal(new ClaimsIdentity(
         [
-            new (ClaimTypes.NameIdentifier, "1"),
+            new(ClaimTypes.NameIdentifier, TestUserId.ToString()),
         ], "mock"));
 
         _controller.ControllerContext = new ControllerContext
@@ -35,18 +36,32 @@ public class StockAccountControllerTests
     }
 
     [Fact]
-    public async Task GetAllAccounts_ReturnsOkResult_WithAccounts()
+    public async Task GetAllAccounts_ReturnsNotFound_WhenNoAccounts()
     {
         // Arrange
-        var userId = 1;
-        List<AvailableAccount> accounts = [new(1, "Test Account")];
-        _mockStockAccountRepository.Setup(repo => repo.GetAvailableAccounts(userId)).Returns(accounts.ToAsyncEnumerable());
+        _mockStockAccountRepository.Setup(repo => repo.GetAvailableAccounts(TestUserId))
+            .Returns(new List<AvailableAccount>().ToAsyncEnumerable());
 
         // Act
         var result = await _controller.Get();
 
         // Assert
-        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task GetAllAccounts_ReturnsOkResult_WithAccounts()
+    {
+        // Arrange
+        List<AvailableAccount> accounts = [new(1, "Test Account")];
+        _mockStockAccountRepository.Setup(repo => repo.GetAvailableAccounts(TestUserId))
+            .Returns(accounts.ToAsyncEnumerable());
+
+        // Act
+        var result = await _controller.Get();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
         var returnValue = Assert.IsType<List<AvailableAccount>>(okResult.Value);
         Assert.Single(returnValue);
     }
@@ -55,9 +70,8 @@ public class StockAccountControllerTests
     public async Task GetAccount_ReturnsOkResult_WithAccount()
     {
         // Arrange
-        var userId = 1;
         var accountId = 1;
-        StockAccount account = new(userId, accountId, "Test Account");
+        StockAccount account = new(TestUserId, accountId, "Test Account");
         _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync(account);
 
         // Act
@@ -70,13 +84,43 @@ public class StockAccountControllerTests
     }
 
     [Fact]
+    public async Task GetAccount_ReturnsForbid_WhenUserDoesNotOwnAccount()
+    {
+        // Arrange
+        var accountId = 1;
+        var differentUserId = 999;
+        StockAccount account = new(differentUserId, accountId, "Test Account");
+        _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync(account);
+
+        // Act
+        var result = await _controller.Get(accountId);
+
+        // Assert
+        Assert.IsType<ForbidResult>(result);
+    }
+
+    [Fact]
+    public async Task GetAccount_ReturnsNoContent_WhenAccountNotFound()
+    {
+        // Arrange
+        var accountId = 1;
+        _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync((StockAccount?)null);
+
+        // Act
+        var result = await _controller.Get(accountId);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
     public async Task AddAccount_ReturnsOkResult_WithNewAccount()
     {
         // Arrange
-        var userId = 1;
         var newAccountId = 1;
         AddAccount addAccount = new("New Account");
-        _mockStockAccountRepository.Setup(repo => repo.Add(userId, addAccount.accountName)).ReturnsAsync(newAccountId);
+        _mockStockAccountRepository.Setup(repo => repo.Add(TestUserId, addAccount.accountName))
+            .ReturnsAsync(newAccountId);
 
         // Act
         var result = await _controller.Add(addAccount);
@@ -87,22 +131,53 @@ public class StockAccountControllerTests
     }
 
     [Fact]
-    public async Task DeleteAccount_ReturnsOkResult_WithDeletedAccount()
+    public async Task DeleteAccount_ReturnsOkResult_WithSuccess()
     {
         // Arrange
-        var userId = 1;
         var accountId = 1;
         DeleteAccount deleteAccount = new(accountId);
-        StockAccount account = new(userId, accountId, "Test Account");
+        StockAccount account = new(TestUserId, accountId, "Test Account");
         _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync(account);
         _mockStockAccountRepository.Setup(repo => repo.Delete(accountId)).ReturnsAsync(true);
+        _mockStockAccountEntryRepository.Setup(repo => repo.Delete(accountId)).ReturnsAsync(true);
 
         // Act
         var result = await _controller.Delete(deleteAccount.accountId);
 
         // Assert
         var okResult = Assert.IsType<OkObjectResult>(result);
-        Assert.IsType<bool>(okResult.Value);
-        Assert.True((bool)okResult.Value);
+        Assert.True((bool)okResult.Value!);
+    }
+
+    [Fact]
+    public async Task DeleteAccount_ReturnsForbid_WhenUserDoesNotOwnAccount()
+    {
+        // Arrange
+        var accountId = 1;
+        var differentUserId = 999;
+        DeleteAccount deleteAccount = new(accountId);
+        StockAccount account = new(differentUserId, accountId, "Test Account");
+        _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync(account);
+
+        // Act
+        var result = await _controller.Delete(deleteAccount.accountId);
+
+        // Assert
+        Assert.IsType<ForbidResult>(result);
+    }
+
+    [Fact]
+    public async Task DeleteAccount_ReturnsNotFound_WhenAccountDoesNotExist()
+    {
+        // Arrange
+        var accountId = 1;
+        DeleteAccount deleteAccount = new(accountId);
+        _mockStockAccountRepository.Setup(repo => repo.Get(accountId)).ReturnsAsync((StockAccount?)null);
+
+        // Act
+        var result = await _controller.Delete(deleteAccount.accountId);
+
+        // Assert
+        Assert.IsType<NotFoundObjectResult>(result);
     }
 }
