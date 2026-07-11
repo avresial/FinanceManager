25a5f26f6876b57d2dcf8b8f635e1279d39b2f0e
Mikołaj Karkowski <mikikarkowski@gmail.com>
2026-07-11 21:46:12 +0200

    Configure local test user

--- code/FinanceManager.Application/Identity/Seeders/TestUserAccountSeeder.cs
+++ code/FinanceManager.Application/Identity/Seeders/TestUserAccountSeeder.cs
@@ -9,7 +9,7 @@ namespace FinanceManager.Application.Identity.Seeders;
 public class TestUserAccountSeeder(IUserRepository userRepository, IConfiguration configuration,
     ILogger<TestUserAccountSeeder> logger) : ISeeder
 {
-    private const string _defaultTestUserName = "testuser";
+    private const string _defaultTestUserName = "testuser@localhost";
 
     public async Task Seed(CancellationToken cancellationToken = default)
     {
